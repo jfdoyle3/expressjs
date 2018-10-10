4f9da50fb99070f7cd0bf0617d98a35a9235bcd1
Jim Doyle <indellibleink@gmail.com>
2018-10-10 13:37:39 -0400

    Updated files with tutorial

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 var express=require('express');
 var app=express();
 
-app.get('/', function(request, response){
-    response.send("Resistanse is Futile!!");
-});
+//app.get('/', function(request, response){
+// response.send("Resistanse is Futile!!"); 
+//   response.sendFile(__dirname+'/public/index.html');
+//});
+
+app.use(express.static('public'));
 
 app.listen(8079, function(){
     console.log('port open');
